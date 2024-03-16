3f77c4fab11c483445f3e8a35053eb29231f50c8
Alex Testa <alex.testa@studio.unibo.it>
2024-03-16 13:06:05 +0100

    Bug Fix

--- src/main/scala/Task2/Exercise.scala
+++ src/main/scala/Task2/Exercise.scala
@@ -1,33 +1,31 @@
 package Task2
 
-import junit.framework.Test
-
 object Exercise extends App:
 
-    //Exercise 3a
+    //Task 3a
     def positive(x: Int): String = x match
         case x if x >= 0 => "positive"
         case _ => "negative"
 
-    //Exercise 3b - 3c
+    //Task 3b - 3c
     def neg[X](pred: X => Boolean): (X => Boolean) = (element => !pred(element))
 
-    //Exercise 4
+    //Task 4
     val p1: Integer => Integer => Boolean => Boolean = x => y => z => x <= y == z
     val p2: (Integer, Integer, Boolean) => (Boolean) = ((x,y,z) => (x <= y == z)) 
     def p3(x: Integer)(y: Integer)(z: Boolean): Boolean = x <= y == z
     def p4(x: Integer, y:Integer, z:Boolean): Boolean = x <= y == z
 
-    //Exercise 5
+    //Task 5
     def compose[A, B, C](f: (B => C), g: (A => B)): (A => C) = (number => f(g(number)))
 
-    //Exercise 6
+    //Task 6
     @annotation.tailrec
     def gcd(a: Int, b: Int): Int = b match
         case b if b == 0 => a
         case _ => gcd(b, a % b) 
     
-    //Exercise 7
+    //Task 7
     enum Shape:
         case Rectangle(widht:Double, height:Double)
         case Circle(radius: Double)
@@ -44,5 +42,31 @@ object Exercise extends App:
             case Rectangle(widht, height) => Rectangle(widht * alpha, height * alpha)
             case Circle(radius) => Circle(radius * alpha)
             case Square(side) => Square(side * alpha)
+
+    //Task 8
+    object Optionals:
+        enum Optional[A]:
+            case Maybe(value: A)
+            case Empty()
+
+    import Optionals.Optional
+    import Optionals.Optional.*
+
+    object Optional:
+        def isEmpty[A](optional: Optional[A]): Boolean = optional match
+            case Empty() => true
+            case _ => false
+
+        def orElse[A, B >: A](optional: Optional[A], default: B): B = optional match
+            case Maybe(value) => value
+            case Empty() => default
+
+        def map[A, B](optional: Optional[A], f: A => B): Optional[B] = optional match
+            case Maybe(x) => Maybe(f(x))
+            case _ => Empty()
+
+        def filter[A](optional: Optional[A])(predicate: A => Boolean): Optional[A] = optional match
+            case Maybe(x) if predicate(x) => Maybe(x)
+            case _ => Empty()
 
 end Exercise
